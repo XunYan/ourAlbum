4840d244aff6948d09f90040dbfe595867b247ab
XunYan <13488858795@163.com>
2017-09-01 17:22:56 +0800

    yx

--- src/components/album_all.vue
+++ src/components/album_all.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    全部相册
+    <ul class="album_container" v-if="album_list.length !== 0">
+      <li v-for="val in album_list"><img src="../assets/floder.jpg" alt="">{{val.name}}</li>
+    </ul>
+    <div class="create_album" v-else="">创建相册</div>
   </div>
 </template>
 
@@ -8,10 +11,44 @@
     export default {
         data() {
           return {
-
+            album_list:[
+              {
+                name:"123"
+              },
+              {
+                name:"123"
+              },
+              {
+                name:"123"
+              },
+              {
+                name:"123"
+              },
+              {
+                name:"123"
+              },
+            ]
           }
         }
     }
 </script>
 
-<style scoped=""></style>
+<style scoped="">
+  .album_container{
+    padding: 1rem;
+    display: flex;
+    flex-flow: wrap;
+    justify-content: space-around;
+  }
+  .album_container li{
+    height: 10rem;
+    width: 10rem;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+  .album_container li img{
+    width: 10rem;
+    height: 8rem;
+  }
+</style>
